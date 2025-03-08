1c2f9449792c291bcdb8447d9f01836d3bcde9c3
MilenaRickliS <milena.silverio2506@gmail.com>
2025-03-07 21:47:43 -0300

    cc

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+//classe principal
+class LayoutDemo extends StatelessWidget
+{
+  Widget build(BuildContext context){
+    return Scaffold(
+      //APP BAR
+      appBar: AppBar(
+        title: Text("Exemplo de Layout"),
+        backgroundColor: Colors.blue.shade700,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
         ),
+      ),
+      //body da pagina
+      body: Column( //organiza em colunas
+        mainAxisSize: MainAxisSize.max, //TAMANHO MAX DA TELA
+        crossAxisAlignment: CrossAxisAlignment.center, //todos os componentes centralizados
+        children: <Widget>[
+          //seção de perfil 
+          Container(
+            padding: EdgeInsets.all(16.0),
+            margin: EdgeInsets.symmetric(horizontal: 16.0),
+            //cor de fundo
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LayoutDemo(),
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+      scaffoldBackgroundColor: Colors.white,
+    ),
+  ));
 }
